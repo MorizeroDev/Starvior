b225a28b35b86ac9f8ab7b4a3410139c92ed038d
buger404 <ris_vb@126.com>
2021-05-04 23:52:29 +0800

    修复鬼畜移动卡住；修改植物阻碍移动问题；将旧剧本的资源移走

--- code/Morizero/Assets/Map/Chara.cs
+++ code/Morizero/Assets/Map/Chara.cs
@@ -4,44 +4,53 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
+// 角色控制器
 public class Chara : MonoBehaviour
 {
+    // 朝向枚举
     public enum walkDir{
         Down,Left,Right,Up
     }
-    private Sprite[] Animation;
-    public string Character;
-    public bool Controller = false;
-    private SpriteRenderer image;
-    public walkDir dir;
-    private bool walking;
-    private int walkBuff = 1;
-    private float walkspan;
-    private float sx,sy,ex,ey;
-    private float tx,ty,lx,ly;
-    public GameObject MoveArrow;
-    private bool tMode = false;
+    
+    private Sprite[] Animation;                 // 行走图图像集
+    public string Character;                    // 对应的人物
+    public bool Controller = false;             // 是否为玩家
+    private SpriteRenderer image;               // 图形显示容器
+    public walkDir dir;                         // 当前朝向
+    private bool walking;                       // 是否正在行走
+    private int walkBuff = 1;                   // 行走图系列帧序数
+    private float walkspan;                     // 行走图动画间隔控制缓冲
+    private float sx,sy,ex,ey;                  // 地图边界x,y - x,y
+    private float tx,ty,lx,ly;                  // 目标地点x,y；上一次的坐标x,y
+    public GameObject MoveArrow;                // 点击移动反馈
+    private bool tMode = false;                 // 点击移动模式
 
     private void Awake() {
+        // 载入行走图图像集，并初始化相关设置
         Animation = Resources.LoadAll<Sprite>("Players\\" + Character);
         image = this.GetComponent<SpriteRenderer>();
         dir = walkDir.Down;
         UploadWalk();
+        // 获取地图边界并处理
         Vector3 size = new Vector3(0.25f,0.25f,0f);
         Vector3 pos = GameObject.Find("startDot").transform.localPosition;
         sx = pos.x + size.x; sy = pos.y - size.y; 
         pos = GameObject.Find("endDot").transform.localPosition;
         ex = pos.x - size.x; ey = pos.y + size.y * 1.7f; 
+        // 如果是玩家则绑定至MapCamera
         if(Controller) MapCamera.Player = this;
+        // 如果是玩家并且传送数据不为空，则按照传送设置初始化
         if(Controller && MapCamera.initTp != -1){
             dir = MapCamera.initDir;
             UploadWalk();
+            // 取得传送位置坐标
             this.transform.localPosition = GameObject.Find("tp" + MapCamera.initTp).transform.localPosition;
         }
     }
-
+    // 更新行走图图形
     void UploadWalk(){
         if(walking){
+            // 行走时的图像
             walkspan += Time.deltaTime;
             if(walkspan > 0.1f){
                 walkBuff ++;
@@ -50,22 +59,30 @@ public class Chara : MonoBehaviour
             }
             walking = false;
         }else{
+            // 未行走时
             walkspan = 0;
             walkBuff = 1;
         }
+        // 设定帧
         image.sprite = Animation[(int)dir * 3 + walkBuff];
     }
     void FixedUpdate()
     {
+        // 如果不是玩家
         if(!Controller) return;
+        // 如果剧本正在进行则退出
         if(MapCamera.SuspensionDrama) return;
 
+        // 如果屏幕被点击
         if(Input.GetMouseButton(0)){
+            // 从屏幕坐标换算到世界坐标
             Vector3 mpos = MapCamera.mcamera.GetComponent<Camera>().ScreenToWorldPoint(Input.mousePosition);
             mpos.z = 0;
-            // 检测点击的位置是否是地板
+            // 检查是否点击的是UI而不是地板
             if (EventSystem.current.IsPointerOverGameObject()) return;
+            // 设置相关参数
             tMode = true;tx = mpos.x;ty = mpos.y;lx = 0;ly = 0;
+            // 设置点击反馈
             MoveArrow.transform.localPosition = mpos;
             MoveArrow.SetActive(true);
         }
