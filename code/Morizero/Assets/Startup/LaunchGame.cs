93e178211949aeead0cf19035af8925a23bc21e7
ris_vb@126.com <ris_vb@126.com>
2021-07-10 23:27:09 +0800

    redesign the Startup Scene; update articles; update soundtracks

---
+++ code/Morizero/Assets/Startup/LaunchGame.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaunchGame : MonoBehaviour
+{
+    private void Update() {
+        if(Input.GetMouseButtonUp(0)){
+            Switcher.Carry("ROOM_XUELAN");
+        }
+    }
+}
