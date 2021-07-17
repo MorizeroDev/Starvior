2a1da6e87a47fde0ce950bf9c3320c9d96adcf36
ris_vb@126.com <ris_vb@126.com>
2021-07-17 18:58:24 +0800

    add transwall;fix door hide problem;fix UI collider disturb mapsystem

--- code/Morizero/Assets/Map/Door.cs
+++ code/Morizero/Assets/Map/Door.cs
@@ -7,15 +7,18 @@ public class Door : CheckObj
     public int TPPort = 1;
     public Chara.walkDir TPDirection = Chara.walkDir.Down;
     public string TargetMap = "";
+    private bool selfHide = false;
 
     public override void Update() {
         if(IsActive()){
             MapCamera.initTp = TPPort;MapCamera.initDir = TPDirection;
-            this.transform.parent.GetComponent<SpriteRenderer>().color = new Color(0,0,0,0);
-            this.transform.localPosition = new Vector3(-10000,-10000,-100);
-            MapCamera.HitCheck = null;
+            //this.transform.parent.GetComponent<SpriteRenderer>().color = new Color(0,0,0,0);
+            //this.transform.localPosition = new Vector3(-10000,-10000,-100);
+            selfHide = true;
             this.GetComponent<AudioSource>().Play();
             Switcher.Carry(TargetMap);
         }
+        if(selfHide)
+            MapCamera.HitCheck = null;
     }
 }
